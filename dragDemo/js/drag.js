05fe477833aa6eb19777c9296fb8d02b1854c047
webproblem <yqx_emails@163.com>
2017-04-21 11:26:22 +0800

    优化拖拽功能

--- dragDemo/js/drag.js
+++ dragDemo/js/drag.js
@@ -1,88 +1,129 @@
-;(function(window,document){
-    var eventUtil = {
-	    getElement: function(opt){
-		    var allTag = Array.prototype.slice.call(document.querySelectorAll("*"));
-			var eles = [];
-			allTag.forEach(function(v,i){
-			    if(allTag[i].getAttribute(opt)){
-				    eles.push(allTag[i]);    
+(function(window,document,undefined){
+    var _DragView = DragView;
+    var DragView = function(options){
+	    this.defaultSettings = {
+		    "dragWrap": document.body,
+			"dragableArea": document.body,
+			"isDrag": true,
+			"isDummyWrap": true,
+			"dummyWrapClass": "dragview_dummy_wrap",
+			"dragStatus": false
+		};
+		this.options = this.extend(this.defaultSettings,options);
+		this.options.tempDragWrap = this.options.dragWrap;
+		this.options.dragableArea = this.options.dragableArea ? this.options.dragableArea : this.options.dragWrap;
+        this.init();
+    }
+	DragView.prototype = {
+	        "constructor": DragView,
+		    "init": function(){
+			    var that = this;
+				var dragMove = document.createElement("div");
+				dragMove.setAttribute("class","dragview_move");
+				document.body.appendChild(dragMove);
+				that.options.isDrag && (that.options.dragableArea.style.cursor = "move");
+				that.options.dragableArea.onmousedown = function(e){
+				    if(!that.options.dragStatus){
+						that.throttle(function(){
+							that.options.isDrag && that.mouseDown(e);
+						},null,0);
+					}
+				}.bind(that);
+		    },
+			"getEvent": function(e){
+			    return e ? e : window.event;
+			},
+		    "extend": function(setting,option){
+		      for(var attr in setting){
+			      if(typeof option[attr] != "undefined"){
+				      setting[attr] = option[attr];
+				  }
+			  }
+			  return setting;
+		    },
+			"throttle": function(fn,context,delay){
+			    clearTimeout(fn.timeoutId);
+				fn.timeoutId = setTimeout(function(){
+					fn.call(context);
+				},delay);
+			},
+		    "mouseDown": function(e){
+				var that = this;
+				var dragMoveArea = "";
+				var dragWrap = that.options.dragWrap;
+				var events = that.getEvent(e);
+				var disX = events.clientX - dragWrap.offsetLeft;
+				var disY = events.clientY - dragWrap.offsetTop;
+				document.getElementsByClassName("dragview_move")[0].style.display = "block";
+				dragWrap.style.position = "absolute";
+				dragWrap.style.zIndex = "99999";
+				that.options.tempDragWrap = that.options.dragWrap;
+				if(that.options.isDummyWrap){
+					dragMoveArea = document.createElement("div");
+					dragMoveArea.setAttribute("class",that.options.dummyWrapClass);
+					dragMoveArea.style.width = dragWrap.clientWidth + "px";
+					dragMoveArea.style.height = dragWrap.clientHeight + "px";
+					dragMoveArea.style.position = "absolute";
+					dragMoveArea.style.zIndex = "99999";
+					dragMoveArea.style.top = dragWrap.style.top;
+					dragMoveArea.style.left = dragWrap.style.left;
+					document.body.appendChild(dragMoveArea);
+					that.options.tempDragWrap = dragMoveArea;
+                }				
+				that.options.dragStatus = true;
+				document.onmousemove = function(e){
+					that.throttle(function(){
+					    var _events = that.getEvent(e);
+					    that.mouseMove(_events,disX,disY,dragMoveArea);
+					},null,0);
 				}
-			})
-			return eles;
-		}, 
-        getEvent: function(e){
-        	return e ? e : window.event;
-        }
-    };
-    var Drag = function(){
-        this.contentElement = "";
-        this.dragElement = "";
-        this.isDrag = true;
-    }
-    Drag.prototype.mouseDown = function(e,option){
-    	var that = this;
-        that.contentElement = option.contentElement;
-        that.dragElement = option.dragElement;
-        var events = eventUtil.getEvent(e);
-        var disX = events.clientX - that.contentElement.offsetLeft;
-        var disY = events.clientY - that.contentElement.offsetTop;
-        that.contentElement.style.position = "absolute";
-        that.contentElement.style.zIndex = "99999";
-        that.isDrag = true;
-        document.onmousemove = function(e){
-        	var _events = eventUtil.getEvent(e);
-        	that.mouseMove(_events,disX,disY);
-        }
-        document.onmouseup = function(){
-            that.mouseUp();
-        }
-    }
-    Drag.prototype.mouseMove = function(_events,disX,disY){
-        if(this.isDrag){
-        	var _x = _events.clientX - disX;
-        	var _y = _events.clientY - disY;
-            var _winW = document.documentElement.clientWidth || document.body.clientWidth;
-            var _winH=document.documentElement.clientHeight || document.body.clientHeight;
-            var option = {
-                "x": _x,
-                "y": _y,
-                "winX": _winW,
-                "winY": _winH,
-                "dragW": this.contentElement.offsetWidth,
-                "dragH": this.contentElement.offsetHeight 
-            };
-            this.limiteRange(option);
-        }
-    }
-    Drag.prototype.mouseUp = function(){
-        this.isDrag = false;
-    }
-    Drag.prototype.limiteRange = function(option){
-        //document.title = option.x + "," + option.y;
-        if(option.x <= 0){
-            this.contentElement.style.left = "0px";
-        }else if((option.x + option.dragW) >= option.winX){
-            this.contentElement.style.left = (option.winX - option.dragW) + "px";
-        }else{
-            this.contentElement.style.left = option.x + "px";
-        }
+				document.onmouseup = function(){
+					that.options.dragStatus && that.mouseUp(dragMoveArea);
+				}
+			},
+			"mouseMove": function(_events,disX,disY,dragMoveArea){
+				if(this.options.dragStatus){
+					var _x = _events.clientX - disX;
+					var _y = _events.clientY - disY;
+					var _winW = document.documentElement.clientWidth || document.body.clientWidth;
+					var _winH=document.documentElement.clientHeight || document.body.clientHeight;
+					var option = {
+						"x": _x,
+						"y": _y,
+						"winX": _winW,
+						"winY": _winH,
+						"dragW": this.options.tempDragWrap.offsetWidth,
+						"dragH": this.options.tempDragWrap.offsetHeight
+					};
+					this.limiteRange(option);
+				}
+			},
+			"mouseUp": function(dragMoveArea){
+			    this.options.dragWrap.style.left = this.options.tempDragWrap.style.left;
+				this.options.dragWrap.style.top = this.options.tempDragWrap.style.top;
+				this.options.dragStatus = false;
+				dragMoveArea!="" && document.body.removeChild(dragMoveArea);
+				document.getElementsByClassName("dragview_move")[0].style.display = "none";
+			},
+			"limiteRange": function(option){
+				if(option.x <= 0 || option.dragW >= option.winX){
+					this.options.tempDragWrap.style.left = "0px";
+				}else if((option.x + option.dragW) >= option.winX){
+					this.options.tempDragWrap.style.left = (option.winX - option.dragW) + "px";
+				}else{
+					this.options.tempDragWrap.style.left = option.x + "px";
+				}
 
-        if(option.y <= 0){
-            this.contentElement.style.top = "0px";
-        }else if((option.y + option.dragH) >= option.winY){
-            this.contentElement.style.top = (option.winY - option.dragH) + "px";
-        }else{
-            this.contentElement.style.top = option.y + "px";
-        }
-    }
-    dragElement = eventUtil.getElement("data-drag");
-	dragElement.forEach(function(v,i){
-	    var Drags = new Drag();
-	    dragElement[i].onmousedown = function(e){
-		    Drags.mouseDown(
-                e,{"contentElement":dragElement[i].parentNode,"dragElement":this}
-			);
-		}
-	})
-	
+				if(option.y <= 0 || option.dragH >= option.winY){
+					this.options.tempDragWrap.style.top = "0px";
+				}
+				else if((option.y + option.dragH) >= option.winY){
+					this.options.tempDragWrap.style.top = (option.winY - option.dragH) + "px";
+				}
+				else{
+					this.options.tempDragWrap.style.top = option.y + "px";
+				}
+			}
+	};
+	window.DragView = DragView;
 })(window,document)
